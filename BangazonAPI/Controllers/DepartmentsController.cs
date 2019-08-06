b57430980dc3c90557cc431998d443e9815574c2
Jake-Preston <prestonjak@outlook.com>
2019-08-06 15:00:04 -0500

    department branch

---
+++ BangazonAPI/Controllers/DepartmentsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public DepartmentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        
+            }
+        }
